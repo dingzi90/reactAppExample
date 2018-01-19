426aa2f77c470f21ebd9f8a2a836f58a2f70a50c
dingzi <906289674@qq.com>
2018-01-19 11:34:18 +0800

    表单

--- src/number1/KanbanBoard.js
+++ src/number1/KanbanBoard.js
@@ -2,12 +2,25 @@ import React, {Component} from 'react';
 import List from './List';
 
 class KanbanBoard extends Component {
+	handleSubmit(event){
+		console.log("Submitted values are:",event.target.name.value,event.target.email.value);
+		event.preventDefault();
+	}
 	render(){
 		return (
 			<div className="app">
 				<List id="todo"  title="To Do"  cards={this.props.cards.filter((card) =>card.status === "todo")} />
 				<List id="in-progress"  title="In progress"  cards={this.props.cards.filter((card) =>card.status === "in-progress")} />
 				<List id="done"  title="Done"  cards={this.props.cards.filter((card) =>card.status === "done")} />
+				<form  onSubmit={this.handleSubmit}>
+					<div className="formGroup">
+							Name:<input name="name"  type="text"/>
+					</div>
+					<div  className="formGroup">
+							Email:<input name="email"  type="mail"/>
+					</div>
+					<button  type="submit">Submit</button>,
+				</form>
 			</div>
 		);
 	}
